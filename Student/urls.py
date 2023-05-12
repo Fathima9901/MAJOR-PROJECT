517bf2b67100ec5287f6cc25490466ccb9a47059
Fathima9901 <106338530+Fathima9901@users.noreply.github.com>
2023-05-12 21:41:18 +0530

    Add files via upload

---
+++ Student/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('register', views.register, name='register'),
+    path('sign_up', views.sign_up, name='sign_up'),
+    path('login_view', views.login_view, name='login_view'),
+    path('student_dashboard', views.student_dashboard, name='student'),
+]
+
